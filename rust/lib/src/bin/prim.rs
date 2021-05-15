f53186813ed9f3abeb599e47fcd8fa1bbf6e6fca
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-05-16 03:09:32 +0900

    fix

--- rust/lib/src/bin/prim.rs
+++ rust/lib/src/bin/prim.rs
@@ -1,4 +1,9 @@
 struct Prim {}
 impl Prim {
-    fn init(graph: Vec<Vec<isize>>)
+    fn init(graph: Vec<Vec<isize>>){
+        println!("{:?}",graph);
+    }
+}
+fn main(){
+    Prim::init(vec![vec![0;1];1]);
 }
